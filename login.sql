701eb860135cdbc60bc4eb25866b04480c438c74
Paulwin <dpaulwinjeba@karunya.edu.in>
2017-03-16 00:57:05 +0530

    Update login.sql

--- login.sql
+++ login.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2017 at 02:19 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Mar 15, 2017 at 08:25 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `login`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `registerno` varchar(20) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `dept` varchar(40) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`registerno`, `name`, `dept`, `status`) VALUES
+('UR14CS179', 'D PAULWIN JEBA', 'COMPUTER SCIENCE AND ENGINEERING', 'REGISTERED'),
+('UR14BT013', 'NEETU MANOJ', 'BIOTECHNOLOGY AND ENGINEERING', 'REGISTERED');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `rno` varchar(20) NOT NULL,
+  `cdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`rno`, `cdate`) VALUES
+('', '2017-03-16 00:34:30'),
+('UR14CS179', '2017-03-16 00:30:51');
 
 -- --------------------------------------------------------
 
@@ -43,6 +83,12 @@ INSERT INTO `users` (`username`, `password`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD UNIQUE KEY `rno` (`rno`);
 
 --
 -- Indexes for table `users`
